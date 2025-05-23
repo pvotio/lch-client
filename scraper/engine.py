627e70d103b7e093ee3c42a2e34e17b1cc3cd3c3
Ali Moghimi <mghmiali@gmail.com>
2025-05-23 22:11:39 +0000

    feature: new logic to fetch csc price data

--- scraper/engine.py
+++ scraper/engine.py
@@ -1,68 +1,65 @@
-from typing import List
-
 import pandas as pd
 import requests
+from bs4 import BeautifulSoup
 
 from config import logger
 from utils import Request
 
 
 class Engine:
-    def __init__(self, url: str, max_retries: int, backoff_factor: int) -> None:
-        self.url = url
-        self.request = Request(max_retries=max_retries, backoff_factor=backoff_factor)
 
-    def fetch(self, content: str = None) -> pd.DataFrame:
-        logger.debug(f"Attempting to fetch content from {self.url}.")
-        if not content:
-            content = self.get_content()
-            logger.debug(
-                f"Successfully fetched content from {self.url}. Now parsing the content."  # noqa: E501
-            )
-        else:
-            logger.debug(
-                "Successfully loaded the content from the user provided argument. Now parsing the content."  # noqa: E501
-            )
+    BASE_URL = "https://www.lseg.com/en/post-trade/clearing/lch-services/cdsclear/essentials/pricing-data"  # noqa: E501
 
-        dfs = self.parse_html(content)
-        logger.debug(f"\n{dfs}")
-        self.validate_data(dfs)
-        df = self.concat_tables(dfs)
+    def __init__(self, max_retries, backoff_factor):
+        self.data = {}
+        self.dfs = {}
+        self.request = Request(
+            max_retries=max_retries, backoff_factor=backoff_factor
+        ).request
+
+    def fetch(self) -> pd.DataFrame:
+        self.fetch_api_endpoints()
+        self.get_content()
+        self.convert_to_df()
+        self.validate_data()
+        df = self.concat_tables()
         logger.debug(f"\n{df}")
-        logger.info(f"Parsed content from {self.url}. Extracted {len(df)} rows.")
+        logger.info(f"Extracted {len(df)} rows.")
         return df
 
     def get_content(self) -> str:
-        try:
-            data = {"form_id": "lch_data_token_cookie_accept_terms", "op": "Accept"}
-            r = self.request.request("POST", self.url, data=data)
-            r.raise_for_status()
-            return r.text
-        except requests.RequestException as e:
-            logger.error(f"Error fetching content from {self.url}. Error: {e}")
-            raise ConnectionError(f"Failed to connect to {self.url}.") from e
+        for url in self.api_endpoints:
+            try:
+                r = self.request("GET", url)
+                r.raise_for_status()
+                self.data[url.split("/")[-1]] = (
+                    r.json()["Data"] if "Data" in r.json() else r.json()
+                )
+            except requests.RequestException as e:
+                logger.error(f"Error fetching content from {url}. Error: {e}")
 
-    def parse_html(self, content: str) -> pd.DataFrame:
-        try:
-            dfs = pd.read_html(content)
-        except Exception as e:
-            logger.error(f"Error parsing HTML via pandas. Error: {e}")
-            raise e
+    def convert_to_df(self):
+        for key, data in self.data.items():
+            self.dfs[key] = pd.DataFrame(data)
 
-        if len(dfs):
-            logger.debug(
-                f"Successfully parsed content from {self.url}. Extracted {len(dfs)} tables."  # noqa: E501
-            )
-            return dfs
-        else:
-            raise ValueError(f"No data found when parsing content from {self.url}.")
-
-    def validate_data(self, dfs: List[pd.DataFrame]) -> None:
-        if not all([len(df) for df in dfs]):
+    def validate_data(self):
+        if not all([len(df) for df in self.dfs]):
             logger.error("Data validation failed")
             raise ValueError("No price data provided by the provider as of now.")
 
         logger.debug("Data validation succeeded.")
 
-    def concat_tables(self, dfs: List[pd.DataFrame]) -> pd.DataFrame:
-        return pd.concat(dfs[:2], axis=0).reset_index(drop=True)
+    def fetch_api_endpoints(self):
+        resp = self.request("GET", self.BASE_URL)
+        bs4 = BeautifulSoup(resp.text, features="lxml")
+        divs = bs4.find_all("div", attrs={"data-rehydratable": "DataGridEnterprise"})
+        self.api_endpoints = [
+            self.BASE_URL.split("/en")[0] + div["data-api-url"] for div in divs
+        ]
+
+        for url in self.api_endpoints:
+            logger.info(url)
+
+    def concat_tables(self):
+        # Filter out third table on Pricing Data page
+        return pd.concat(list(self.dfs.values())[:2], axis=0).reset_index(drop=True)
